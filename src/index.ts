c4febff80f6ad0a95af35d475224b6f2ac812496
Ryosuke Takahashi <rjooske@gmail.com>
2024-10-26 18:38:50 +0900

    display email addresses along with student ids

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ function mustGetElementById(id: string): HTMLElement {
 
 type In1 = [string, string][];
 type In2 = [string][];
+type In3 = [string, string, string][];
 
 function parseIn1(s: string): In1 | undefined {
   const rows = parse(s);
@@ -50,9 +51,47 @@ function parseIn2(s: string): In2 | undefined {
   return rows;
 }
 
+function parseIn3(s: string): In3 | undefined {
+  const rows = parse(s);
+  if (!Array.isArray(rows)) {
+    return undefined;
+  }
+  for (const row of rows) {
+    if (
+      !(
+        Array.isArray(row) &&
+        row.length === 3 &&
+        typeof row[0] === "string" &&
+        typeof row[1] === "string" &&
+        typeof row[2] === "string"
+      )
+    ) {
+      return undefined;
+    }
+  }
+  return rows;
+}
+
+function displayStudents(
+  studentIds: string[],
+  studentIdToEmail: Map<string, [string, string]>,
+): string {
+  return studentIds
+    .map((id) => {
+      const email = studentIdToEmail.get(id);
+      if (email === undefined) {
+        return `${id} (email unknown)`;
+      } else {
+        return `${id} ${email[0]} ${email[1]}`;
+      }
+    })
+    .join("\n");
+}
+
 function main() {
   const in1Element = mustGetElementById("in1");
   const in2Element = mustGetElementById("in2");
+  const in3Element = mustGetElementById("in3");
   const courseIdElement = mustGetElementById("course-id");
   const verifyElement = mustGetElementById("verify");
   const outElement = mustGetElementById("out");
@@ -60,6 +99,7 @@ function main() {
   assert(
     in1Element instanceof HTMLTextAreaElement &&
       in2Element instanceof HTMLTextAreaElement &&
+      in3Element instanceof HTMLTextAreaElement &&
       courseIdElement instanceof HTMLInputElement &&
       verifyElement instanceof HTMLButtonElement &&
       outElement instanceof HTMLPreElement,
@@ -68,8 +108,14 @@ function main() {
   verifyElement.addEventListener("click", () => {
     const in1 = parseIn1(in1Element.value);
     const in2 = parseIn2(in2Element.value);
+    const in3 = parseIn3(in3Element.value);
     const courseId = courseIdElement.value;
-    assert(in1 !== undefined && in2 !== undefined);
+    assert(in1 !== undefined && in2 !== undefined && in3 !== undefined);
+
+    const studentIdToEmail = new Map<string, [string, string]>();
+    for (const [studentId, schoolEmail, personalEmail] of in3) {
+      studentIdToEmail.set(studentId, [schoolEmail, personalEmail]);
+    }
 
     const studentIdToCourseId = new Map<string, string>();
     for (const [studentId, courseId] of in1) {
@@ -109,9 +155,10 @@ function main() {
     }
 
     outElement.innerHTML = `この授業を取らなくていいのに取っている学生:
-${unnecessaryIds.join("\n")}
+${displayStudents(unnecessaryIds, studentIdToEmail)}
+
 この授業を取らないといけないのに取っていない学生:
-${mistakeIds.join("\n")}
+${displayStudents(mistakeIds, studentIdToEmail)}
 `;
   });
 }
